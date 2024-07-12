02ba86f926de9413958c89305c1e90fb48f3544e
Agnes <agnesoserayn@gmail.com>
2024-07-12 22:39:00 +0300

    deleted js file

--- js/upload_files.js
+++
@@ -1,9 +0,0 @@
-const dropArea = document.querySelector('.drop-selection')
-const listSection = document.querySelector('.listSection')
-const listContainer = document.querySelector('.listContainer')
-const fileSelector = document.querySelector('.fileSelector')
-const fileSelectorInput = document.querySelector('.fileSelectorInput')
-
-//upload files with browse button
-fileSelector.onclick = 
-

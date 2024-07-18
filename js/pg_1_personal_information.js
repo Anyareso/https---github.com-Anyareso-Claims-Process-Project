8625ba659087bcc60fa15511a6dd7c9adb9e89c9
Agnes <agnesoserayn@gmail.com>
2024-07-18 22:31:42 +0300

    deleted code, not useful

--- js/pg_1_personal_information.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('.btn-next').addEventListener('click', function() {
-    const sectionUrl = this.getAttribute('data-section');
-    if (sectionUrl) {
-        window.location.href = sectionUrl;
-    }
-});

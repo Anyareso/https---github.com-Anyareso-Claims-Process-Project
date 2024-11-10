e56ef1e2deda7b6abc49e764b99e416978720508
Dodman101 <dodmanmwaura@gmail.com>
2024-11-10 16:08:20 +0300

    Fixed NameError: name 'secrets' is not defined. Did you forget to import 'secrets'?

--- vc_webform/webform_backend/app.py
+++ vc_webform/webform_backend/app.py
@@ -1,5 +1,6 @@
 import os
 import base64
+import secrets
 from flask import Flask, request, jsonify, render_template, redirect, url_for, send_from_directory, flash
 from pathlib import Path
 from sqlalchemy import create_engine

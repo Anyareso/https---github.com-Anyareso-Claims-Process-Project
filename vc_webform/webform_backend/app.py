b8636887e76782c877f71100a4b3550c11c8ba9d
Dodman101 <dodmanmwaura@gmail.com>
2024-11-17 09:56:01 +0300

    corrected the function definition names for the login, registration and forgot password pages.

--- vc_webform/webform_backend/app.py
+++ vc_webform/webform_backend/app.py
@@ -86,15 +86,15 @@ def thank_you_page():
     return render_template('thank_you.html')  # Serves the thank-you page
 
 @app.route('/login')
-def thank_you_page():
+def login():
     return render_template('login.html')  # Serves the login page
 
 @app.route('/register')
-def thank_you_page():
+def register():
     return render_template('register.html')  # Serves the registration page
 
 @app.route('/forgot-password')
-def thank_you_page():
+def forgot_password():
     return render_template('forgot_password.html')  # Serves the forgot_password page
 
 @app.route('/submit-form', methods=['POST'])
